c08f8fc8ebed993d8793e5c11e8c2447b11877c9
Eugene Marinelli <eugeneiiim@gmail.com>
2010-04-10 22:56:18 -0700

    more android work

--- android/src/com/boredapp/Bored.java
+++ android/src/com/boredapp/Bored.java
@@ -9,21 +9,33 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.URL;
+import java.lang.Runnable;
+import android.os.AsyncTask;
 
 public class Bored extends Activity {
   /** Called when the activity is first created. */
   @Override
-    public void onCreate(Bundle savedInstanceState) {
+  public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
     final TextView tv = (TextView) findViewById(R.id.todoTextView);
-    tv.setText("Click the button to get a thing to do.");
+    tv.setText(getThingToDo());
 
     final Button button = (Button) findViewById(R.id.getTodoButton);
     button.setOnClickListener(new View.OnClickListener() {
-	public void onClick(View v) {
-	  tv.setText(getThingToDo());
+        public void onClick(View v) {
+	  new AsyncTask<Void, Integer, String>() {
+	    protected String doInBackground(Void... _) {
+	      return getThingToDo();
+	    }
+
+	    protected void onProgressUpdate(Integer... progress) {}
+
+	    protected void onPostExecute(String result) {
+	      tv.setText(result);
+	    }
+	  }.execute(null, null);
 	}
       });
   }
@@ -36,12 +48,12 @@ public class Bored extends Activity {
 
       String result = "";
       while ((inputLine = in.readLine()) != null) {
-	result += inputLine;
+        result += inputLine;
       }
 
       in.close();
 
-      return result;
+      return result + ".";
     } catch (IOException e) {
       e.printStackTrace();
       return "Request failed.";
