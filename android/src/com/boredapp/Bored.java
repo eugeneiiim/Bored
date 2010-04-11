8f686dc6bad115a6b4320655578b6d3f74664d2e
Eugene Marinelli <eugeneiiim@gmail.com>
2010-04-10 22:05:06 -0700

    android app work

--- android/src/com/boredapp/Bored.java
+++ android/src/com/boredapp/Bored.java
@@ -2,13 +2,49 @@ package com.boredapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.URL;
 
 public class Bored extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+  /** Called when the activity is first created. */
+  @Override
+    public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    final TextView tv = (TextView) findViewById(R.id.todoTextView);
+    tv.setText("Click the button to get a thing to do.");
+
+    final Button button = (Button) findViewById(R.id.getTodoButton);
+    button.setOnClickListener(new View.OnClickListener() {
+	public void onClick(View v) {
+	  tv.setText(getThingToDo());
+	}
+      });
+  }
+
+  private String getThingToDo() {
+    try {
+      URL url = new URL("http://electric-night-28.heroku.com/");
+      BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+      String inputLine;
+
+      String result = "";
+      while ((inputLine = in.readLine()) != null) {
+	result += inputLine;
+      }
+
+      in.close();
+
+      return result;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "Request failed.";
     }
+  }
 }
