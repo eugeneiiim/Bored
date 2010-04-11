7ff51a4d77bfa5d7c0aa84555660495db93d02ea
Eugene Marinelli <eugeneiiim@gmail.com>
2010-04-10 21:20:01 -0700

    add android project

---
+++ android/src/com/boredapp/Bored.java
@@ -0,0 +1,14 @@
+package com.boredapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Bored extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
